97ba1bb4b76a6223c6a6d2a435a8e8c1a2b0eb8e
harmya <hvbhatt@purdue.edu>
2025-04-07 01:08:35 -0400

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="header">
+        <h1>History Walker</h1>
+        <p>This is the home page</p>
+        <a href="/about">About</a>
+    </div>
+</div>
+
+<style>
+    .header {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<div class="container">
+    <h1>About</h1>
+    <p>This is the about page</p>
+</div>
+
+
